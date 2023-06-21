6f6b4fcb1647af7c38e369960cdfc2eaa0c1435e
antony <antony.philip@tcs.com>
2023-06-21 21:56:14 +0530

    initial commit

---
+++ src/main/java/com/pest/store/pesto/PestoApplication.java
@@ -0,0 +1,13 @@
+package com.pest.store.pesto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PestoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PestoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pest/store/pesto/PestoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pest.store.pesto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PestoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
